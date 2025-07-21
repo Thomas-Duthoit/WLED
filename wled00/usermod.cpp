d05cb8eb5c8fc75410e47661341c1cefa0da99a2
Zatomate <duthoit.thomas9@gmail.com>
2025-07-21 10:38:59 +0200

    WLED with usermod
    Added custom usermod to blink an LED

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -10,10 +10,25 @@
 
 //Use userVar0 and userVar1 (API calls &U0=,&U1=, uint16_t)
 
+#define BLINK_PIN 13
+
+
+bool ledState = false;
+unsigned long lastToggleTime = 0;
+
+
+
+
 //gets called once at boot. Do all initialization that doesn't depend on network here
 void userSetup()
 {
+  pinMode(BLINK_PIN, OUTPUT);
 
+  // On envoie un message au Moniteur Série pour confirmer que notre code s'est bien lancé
+  Serial.println("Hello from usermod.cpp! Pin setup complete.");pinMode(BLINK_PIN, OUTPUT);
+
+  // On envoie un message au Moniteur Série pour confirmer que notre code s'est bien lancé
+  Serial.println("Hello from usermod.cpp! Pin setup complete.");
 }
 
 //gets called every time WiFi is (re-)connected. Initialize own network interfaces here
@@ -25,5 +40,9 @@ void userConnected()
 //loop. You can use "if (WLED_CONNECTED)" to check for successful connection
 void userLoop()
 {
-
+  if (millis() - lastToggleTime > 500) { // Toutes les 1000ms (1 seconde)
+    ledState = !ledState; // On inverse l'état de la LED (allumé -> éteint -> allumé...)
+    digitalWrite(BLINK_PIN, ledState);
+    lastToggleTime = millis(); // On met à jour le moment du dernier changement
+  }
 }
